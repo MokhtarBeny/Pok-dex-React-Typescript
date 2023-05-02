20361abd7bf7625b52a1b4a1dc8df4a94057499c
MokhtarBeny <114168044+MokhtarBeny@users.noreply.github.com>
2023-05-02 16:08:10 +0200

    push firebase + build

--- src/models/mock-pokemon.ts
+++ src/models/mock-pokemon.ts
@@ -16,7 +16,7 @@ export const POKEMONS: Pokemon[] = [
   hp: 80,
   cp: 8,
   picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png",
-  types: ["Feu"],
+  types: ["Plante", "Poison"],
   created: new Date()
  },
  {
@@ -24,91 +24,239 @@ export const POKEMONS: Pokemon[] = [
   name: "Florizarre",
   hp: 112,
   cp: 13,
+  picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png",
+  types: ["Plante", "Poison"],
+  created: new Date()
+ },
+ {
+  id: 4,
+  name: "Salamèche",
+  hp: 48,
+  cp: 6,
+  picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png",
+  types: ["Feu"],
+  created: new Date()
+ },
+ {
+  id: 5,
+  name: "Reptincel",
+  hp: 78,
+  cp: 9,
+  picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/005.png",
+  types: ["Feu"],
+  created: new Date()
+ },
+ {
+  id: 6,
+  name: "Dracaufeu",
+  hp: 115,
+  cp: 15,
+  picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/006.png",
+  types: ["Feu"],
+  created: new Date()
+ },
+ {
+  id: 7,
+  name: "Carapuce",
+  hp: 67,
+  cp: 6,
   picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png",
   types: ["Eau"],
   created: new Date()
  },
  {
-  id: 4,
-  name: "Aspicot",
-  hp: 16,
-  cp: 2,
-  picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/013.png",
-  types: ["Insecte", "Poison"],
-  created: new Date()
- },
- {
-  id: 5,
-  name: "Roucool",
-  hp: 30,
-  cp: 7,
-  picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/016.png",
-  types: ["Normal", "Vol"],
-  created: new Date()
- },
- {
-  id: 6,
-  name: "Rattata",
-  hp: 18,
-  cp: 6,
-  picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/019.png",
-  types: ["Normal"],
-  created: new Date()
- },
- {
-  id: 7,
-  name: "Piafabec",
-  hp: 14,
-  cp: 5,
-  picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/021.png",
-  types: ["Normal", "Vol"],
-  created: new Date()
- },
- {
   id: 8,
-  name: "Abo",
-  hp: 16,
-  cp: 4,
-  picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/023.png",
-  types: ["Poison"],
+  name: "Carabaffe",
+  hp: 84,
+  cp: 10,
+  picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/008.png",
+  types: ["Eau"],
   created: new Date()
  },
  {
   id: 9,
-  name: "Pikachu",
-  hp: 21,
-  cp: 7,
-  picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/025.png",
-  types: ["Electrik"],
+  name: "Tortank",
+  hp: 110,
+  cp: 12,
+  picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/009.png",
+  types: ["Eau"],
   created: new Date()
  },
  {
   id: 10,
-  name: "Sabelette",
-  hp: 19,
-  cp: 3,
-  picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/027.png",
-  types: ["Sol"],
+  name: "Chenipan",
+  hp: 40,
+  cp: 2,
+  picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/010.png",
+  types: ["Insecte"],
   created: new Date()
  },
  {
   id: 11,
-  name: "Mélofée",
-  hp: 25,
-  cp: 5,
-  picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/035.png",
-  types: ["Fée"],
+  name: "Chrysacier",
+  hp: 80,
+  cp: 2,
+  picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/011.png",
+  types: ["Insecte"],
   created: new Date()
  },
  {
   id: 12,
-  name: "Groupix",
-  hp: 17,
-  cp: 8,
-  picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/037.png",
+  name: "Papilusion",
+  hp: 108,
+  cp: 11,
+  picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/012.png",
   types: ["Feu"],
   created: new Date()
- }
+ },
+ {
+    id: 13,
+    name: "Aspicot",
+    hp: 40,
+    cp: 2,
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/013.png",
+    types: ["Insecte", "Poison"],
+    created: new Date()
+},
+{
+    id: 14,
+    name: "Coconfort",
+    hp: 80,
+    cp: 2,
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/014.png",
+    types: ["Insecte", "Poison"],
+    created: new Date()
+},
+{
+    id: 15,
+    name: "Dardargnan",
+    hp: 108,
+    cp: 10,
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/015.png",
+    types: ["Insecte", "Poison"],
+    created: new Date()
+},
+{
+    id: 16,
+    name: "Roucool",
+    hp: 43,
+    cp: 4,
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/016.png",
+    types: ["Vol", "Normal"],
+    created: new Date()
+},
+{
+    id: 17,
+    name: "Roucoups",
+    hp: 69,
+    cp: 7,
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/017.png",
+    types: ["Vol", "Normal"],
+    created: new Date()
+},
+{
+    id: 18,
+    name: "Roucarnage",
+    hp: 99,
+    cp: 9,
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/018.png",
+    types: ["Vol", "Normal"],
+    created: new Date()
+},
+{
+    id: 19,
+    name: "Rattata",
+    hp: 30,
+    cp: 3,
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/019.png",
+    types: ["Normal"],
+    created: new Date()
+},
+{
+    id: 20,
+    name: "Rattatac",
+    hp: 88,
+    cp: 8,
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/020.png",
+    types: ["Normal"],
+    created: new Date()
+},
+{
+    id: 21,
+    name: "Piafabec",
+    hp: 40,
+    cp: 4,
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/021.png",
+    types: ["Normal", "Vol"],
+    created: new Date()
+},
+{
+    id: 22,
+    name: "Rapasdepic",
+    hp: 88,
+    cp: 8,
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/022.png",
+    types: ["Normal", "Vol"],
+    created: new Date()
+},
+{
+    id: 23,
+    name: "Abo",
+    hp: 45,
+    cp: 5,
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/023.png",
+    types: ["Poison"],
+    created: new Date()
+},
+{
+    id: 24,
+    name: "Arbok",
+    hp: 93,
+    cp: 8,
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/024.png",
+    types: ["Poison"],
+    created: new Date()
+},
+{
+    id: 25,
+    name: "Pikachu",
+    hp: 70,
+    cp: 7,
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/025.png",
+    types: ["Electrik"],
+    created: new Date()
+},
+{
+    id: 26,
+    name: "Raichu",
+    hp: 97,
+    cp: 8,
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/026.png",
+    types: ["Electrik"],
+    created: new Date()
+},
+{
+    id: 27,
+    name: "Sabelette",
+    hp: 50,
+    cp: 4,
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/027.png",
+    types: ["Sol"],
+    created: new Date()
+},
+{
+    id: 28,
+    name: "Sablaireau",
+    hp: 91,
+    cp: 7,
+    picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/028.png",
+    types: ["Sol"],
+    created: new Date()
+},
+
+
+
+
 ];
   
 export default POKEMONS;
